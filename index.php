abbe4af5a80b00f8955dd4e3bb31410cc09ce4c8
DGorchakov <71970557+DGorchakov@users.noreply.github.com>
2020-11-01 16:14:10 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,4 @@
 <?php 
-if (isset($_POST[name])) {
-	file_put_contents(filewithdata.txt, $_POST[name]);
-}
+echo "10";
 
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title></title>
-</head>
-<body>
-<form action="index.php" method="post">
-	<input type="text" name="name" placeholder = "Имя">
-	 <input type="email" name="email" placeholder = "Почта">
-	 <input type="submit" name="Отправить">
-</form>
-</body>
-</html>
